251f14a86e0f002db00e9c111f25adfe18e90aff
kosmo <moonsangil@naver.com>
2024-11-11 15:36:49 +0900

    첫번째 커밋

--- src/main/java/com/example/HomeClass.java
+++ src/main/java/com/example/HomeClass.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class HomeClass {
     public static void main(String[] args){
+        System.out.println("이것은 추가된 내용");
         System.out.println("설정된 내용");
     }
 }
