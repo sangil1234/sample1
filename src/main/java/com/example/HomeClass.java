5b88f99694e6c4a265e33977070818aae2fadc09
kosmo <moonsangil@naver.com>
2024-11-13 12:31:49 +0900

    수정됐습니다.

--- src/main/java/com/example/HomeClass.java
+++ src/main/java/com/example/HomeClass.java
@@ -4,5 +4,6 @@ public class HomeClass {
     public static void main(String[] args){
         System.out.println("이것은 추가된 내용");
         System.out.println("설정된 내용");
+        System.out.println("유수현바보");
     }
 }
